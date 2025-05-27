71e2af3110cb5c25ba7ce14bdb06b43159093b76
Bruno Alves <93.bruno@gmail.com>
2025-05-27 14:41:14 +0100

    first commit

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import "dotenv/config";
+import { connectDB } from "./db/connectDB.js";
+import authRoutes from "./routes/auth.route.js";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log("Server is running on port: ", PORT);
+});
---
+++ backend/models/user.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    lastLogin: {
+      type: Date,
+      default: Date.now,
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    resetPasswordToken: String,
+    resetPasswordExpiresAt: Date,
+    verificationToken: String,
+    verificationTokenExpiredAt: Date,
+  },
+  { timestamps: true }
+);
+
+//{timestamps: true} -> createdat and updatedat field will be automatically added into the document
+
+export const User = mongoose.models.user || mongoose.model("User", userSchema);
