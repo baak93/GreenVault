9e4c7e47f97ca0a639bb5864de4b8508a5af32b3
Bruno Alves <93.bruno@gmail.com>
2025-06-06 16:37:14 +0100

    feat: add signup button layout and login redirect link

--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -1,6 +1,8 @@
 import { motion } from "framer-motion";
 import { User, Mail, Lock } from "lucide-react";
 import { useState } from "react";
+import { Link } from "react-router-dom";
+
 import Input from "../components/Input";
 
 function SignupPage() {
@@ -45,6 +47,23 @@ function SignupPage() {
           onChange={(e) => setpassword(e.target.value)}
         />
         {/* Password strength meter */}
+
+        <motion.button
+          className="mt-5 w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white font-bold rounded-lg shadow-lg hover:from-green-600 hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900 trasition duration-200"
+          whileHover={{ scale: 1.02 }}
+          whileTap={{ scale: 0.98 }}
+          type="submit"
+        >
+          Sign Up
+        </motion.button>
+      </div>
+      <div className="px-8 py-4 bg-gray-900 bg-opacity-50 flex justify-center">
+        <p className="text-sm text-gray-400">
+          Already have an account?{" "}
+          <Link to={"/login"} className="text-green-400 hover:underline">
+            Login
+          </Link>
+        </p>
       </div>
     </motion.div>
   );
