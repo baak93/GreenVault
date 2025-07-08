2b77f424abf1a0b3d98d036361483c0bae98da87
Bruno Alves <93.bruno@gmail.com>
2025-07-08 16:00:27 +0100

    feat: implement logout functionality with state reset

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,9 +3,11 @@ import { useAuthStore } from "../store/authStore";
 import { formatDate } from "../utils/date";
 
 const HomePage = () => {
-  const { user } = useAuthStore();
+  const { user, logout } = useAuthStore();
 
-  const handleLogout = () => {};
+  const handleLogout = () => {
+    logout();
+  };
 
   return (
     <motion.div
@@ -67,9 +69,8 @@ const HomePage = () => {
           whileHover={{ scale: 1.05 }}
           whileTap={{ scale: 0.95 }}
           onClick={handleLogout}
-          className="w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white 
-				font-bold rounded-lg shadow-lg hover:from-green-600 hover:to-emerald-700
-				 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900"
+          className="w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white font-bold rounded-lg shadow-lg hover:from-green-600 hover:to-emerald-700
+				 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900 cursor-pointer"
         >
           Logout
         </motion.button>
